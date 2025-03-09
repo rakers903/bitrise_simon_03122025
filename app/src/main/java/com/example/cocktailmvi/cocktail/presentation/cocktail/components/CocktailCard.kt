58860b9cf7d81279906d0838ef55796132221b7f
siakertechcon <simon.akers@techconsulting.net>
2025-03-08 20:36:17 -0500

    Project init

--- app/src/main/java/com/example/cocktailmvi/cocktail/presentation/cocktail/components/CocktailCard.kt
+++ app/src/main/java/com/example/cocktailmvi/cocktail/presentation/cocktail/components/CocktailCard.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,7 +18,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.cocktailmvi.R
 import com.example.cocktailmvi.cocktail.domain.Cocktail
 import com.example.cocktailmvi.cocktail.presentation.cocktail.CocktailAction
 import com.example.cocktailmvi.cocktail.presentation.cocktail.state.CocktailCardState
@@ -26,27 +32,44 @@ fun CocktailCard(
     onAction: (CocktailAction) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    Log.d("State", state.isLiked.toString())
     Surface(
         shadowElevation = 3.dp,
         shape = RoundedCornerShape(6.dp)
     ) {
         Column(
             modifier = modifier
-                .clickable {
-                    onAction(CocktailAction.ToggleLike(state.id))
-                }
+//                .clickable {
+//                    onAction(CocktailAction.ToggleLike(state.id))
+//                }
                 .fillMaxWidth()
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(4.dp)
         )  {
-            Text(
-                text = state.name,
-                style = MaterialTheme.typography.titleLarge
-            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = state.name,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Icon(
+                    painter = painterResource(R.drawable.sharp_bookmark),
+                    contentDescription = "Bookmark Icon",
+                    modifier = Modifier
+                        .size(28.dp)
+                        .clickable {
+                            onAction(CocktailAction.ToggleLike(state.id))
+                        },
+                    tint = if(state.isLiked) colorResource(R.color.yellow) else Color.Gray,
+                )
+            }
             Text(
                 text = state.instructions
             )
-            Text(text = if(state.isLiked) "Liked" else "Not Liked")
+
         }
     }
 }
